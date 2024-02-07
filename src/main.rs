4e0b0ac2522cf06ff02fef2dec9e3c2339d379ba
secretval <108518296+secretval@users.noreply.github.com>
2024-02-07 20:52:07 +0100

    fix: the most easy fix ever

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ use std::{
     io::{stdin, stdout, BufRead, Write},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Command {
+    name: String,
     desc: String,
-    action: fn(args: Vec<String>),
+    action: fn(args: Vec<String>, commmands: HashMap<String, Command>),
 }
 fn main() -> ! {
     let mut commands: HashMap<String, Command> = HashMap::new();
     commands.insert(
         "clear".to_string(),
         Command {
+            name: "clear".to_string(),
             desc: "Clear the screen".to_string(),
-            action: |_| {
+            action: |_, _| {
                 // TODO: Look into this
                 let _ = stdout().write(format!("{esc}[2J{esc}[1;1H", esc = 27 as char).as_bytes());
                 let _ = stdout().flush();
@@ -24,8 +26,9 @@ fn main() -> ! {
     commands.insert(
         "ping".to_string(),
         Command {
+            name: "ping".to_string(),
             desc: "Say pong".to_string(),
-            action: |_| {
+            action: |_, _| {
                 let _ = stdout().write(b"pong\n");
                 let _ = stdout().flush();
             },
@@ -34,10 +37,12 @@ fn main() -> ! {
     commands.insert(
         "help".to_string(),
         Command {
-            desc: "Print help".to_string(),
-            action: |_| {
+            name: "help".to_string(),
+            desc: "Print help message ".to_string(),
+            action: |_, commands: HashMap<String, Command>| {
                 for cmd in commands.values() {
-                    println!("{:?}", cmd);
+                    let _ = stdout().write(format!("{}: {}\n", cmd.name, cmd.desc).as_bytes());
+                    let _ = stdout().flush();
                 }
             },
         },
@@ -48,16 +53,17 @@ fn main() -> ! {
         let _ = stdout().flush();
         let mut buffer = String::new();
         let _ = handler.read_line(&mut buffer);
-        let command = commands.get(buffer.trim());
+        let command = &commands.get(buffer.trim());
         (command
             .unwrap_or(&Command {
+                name: "NOT_FOUND".to_string(),
                 desc: "NOT_FOUND".to_string(),
-                action: |_| {
+                action: |_, _| {
                     let _ = stdout().write(b"Command not found.\n");
                     let _ = stdout().flush();
                 },
             })
             // TODO: implement args
-            .action)(vec![]);
+            .action)(vec![], commands.clone());
     }
 }
